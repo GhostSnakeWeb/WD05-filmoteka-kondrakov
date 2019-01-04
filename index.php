5fe3695549d5774bca9428377d577adeb46dd0af
Vladislav <vladan982004@gmail.com>
2019-01-04 18:03:56 +0500

    Edit is ready

--- index.php
+++ index.php
@@ -110,7 +110,7 @@ if ($result = mysqli_query($link, $query)) {
 					<div class="card__header">
 						<h4 class="title-4"><?=@$film['title'] //@ гасит ошибки. Они не будут видны на странице ?></h4>
 						<div>
-							<a href="edit.php/id=<?=$film['id']?>" class="button button--editsmall">Редактировать</a>
+							<a href="edit.php?id=<?=$film['id']?>" class="button button--editsmall">Редактировать</a>
 							<!--GET запрос на удаление карточки-->
 							<a href="?action=delete&id=<?=$film['id']?>" class="button button--removesmall">Удалить</a>
 						</div>
